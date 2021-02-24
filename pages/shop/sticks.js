8a8cc7c80c368504a56f11ed900b093cf9fefca5
Patrick Furtak <patrick@patrickfurtak.com>
2021-02-23 20:45:53 -0500

    Cleaned up boiler code, added shop route for sticks

---
+++ pages/shop/sticks.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Sticks = () => {
+  return (
+    <>
+      <h1>Arcade Sticks</h1>
+      <Link href='/'>
+        <a>Back</a>
+      </Link>
+    </>
+  );
+};
+
+export default Sticks;
