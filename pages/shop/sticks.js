05bd9e56b5e2a31bc5d6699b13eb8cd647cf4e6a
Patrick Furtak <patrick@patrickfurtak.com>
2021-02-26 14:28:52 -0500

    Started styling product cards

--- pages/shop/sticks.js
+++ pages/shop/sticks.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import { getAllSticks } from '../../lib/fetchSticks';
+import styles from '../../styles/Sticks.module.css';
 
 export async function getStaticProps() {
   const arcadeSticks = await getAllSticks();
@@ -20,15 +21,27 @@ const Sticks = ({ arcadeSticks }) => {
       </Head>
       <h1>Arcade Sticks</h1>
       <br />
-      <ul>
-        {arcadeSticks.stock.map(({ brand, model, pid, price }) => (
-          <Link href={`/shop/${pid}`} key={pid}>
-            <li>
-              {brand} {model} - ${price}
-            </li>
-          </Link>
-        ))}
-      </ul>
+      {arcadeSticks.stock.map(({ brand, model, pid, price, image }) => (
+        <Link href={`/shop/${pid}`} key={pid}>
+          <div className={styles.wrapper}>
+            <div className={styles.product_img}>
+              <img src={image} height='420' width='327' />
+            </div>
+            <div className={styles.product_info}>
+              <div className={styles.product_text}>
+                <h1>{model}</h1>
+                <h2>by {brand}</h2>
+              </div>
+              <div className={styles.product_price_btn}>
+                <p>
+                  $<span className={styles.price_number}>{price}</span>
+                </p>
+                <button type='button'>add to cart</button>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ))}
     </>
   );
 };
